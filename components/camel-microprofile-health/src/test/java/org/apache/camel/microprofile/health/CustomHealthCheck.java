e545bf35d4d4ae3284ded46f26338daa8b7a1d86
James Netherton <jamesnetherton@gmail.com>
2019-10-08 09:13:30 +0100

    WIP

--- components/camel-microprofile-health/src/test/java/org/apache/camel/microprofile/health/CustomHealthCheck.java
+++ components/camel-microprofile-health/src/test/java/org/apache/camel/microprofile/health/CustomHealthCheck.java
@@ -5,15 +5,15 @@ import java.util.Map;
 import org.apache.camel.health.HealthCheckResultBuilder;
 import org.apache.camel.impl.health.AbstractHealthCheck;
 
-public class DummyHealthCheck extends AbstractHealthCheck {
+public class CustomHealthCheck extends AbstractHealthCheck {
 
-    protected DummyHealthCheck() {
-        super("dummy", "dummy");
-        getConfiguration().setEnabled(true);
+    protected CustomHealthCheck() {
+        super("test", "test");
     }
 
     @Override
     protected void doCall(HealthCheckResultBuilder builder, Map<String, Object> options) {
-        builder.up();
+//        builder.detail("foo", "bar");
+//        builder.up();
     }
 }
--- components/camel-microprofile-health/src/test/java/org/apache/camel/microprofile/health/TestHealthCheck.java
+++ components/camel-microprofile-health/src/test/java/org/apache/camel/microprofile/health/TestHealthCheck.java
@@ -3,85 +3,45 @@ package org.apache.camel.microprofile.health;
 import io.smallrye.health.SmallRyeHealth;
 import io.smallrye.health.SmallRyeHealthReporter;
 
-import javax.inject.Inject;
-
-import org.apache.camel.BindToRegistry;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.health.HealthCheckRegistry;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.impl.health.ContextHealthCheck;
 import org.apache.camel.impl.health.RoutePerformanceCounterEvaluators;
 import org.apache.camel.impl.health.RoutesHealthCheckRepository;
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.apache.camel.spi.Registry;
 import org.junit.Test;
 
-//@RunWith(Arquillian.class)
 public class TestHealthCheck {
 
-    @BindToRegistry("dummyHealthCheck")
-    protected DummyHealthCheck dummyHealthCheck = new DummyHealthCheck();
-
-    @Inject
-    SmallRyeHealthReporter reporter;
-
-    @Inject
-    CamelContext camelContext;
-
-    @Deployment
-    public static Archive<?> deployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                // Test class
-                .addClass(TestHealthCheck.class)
-                .addClass(CamelMicroProfileHealthCheck.class)
-                .addClass(CamelContextProducer.class)
-                .addPackage("io.smallrye.health")
-//                .addPackage("io.smallrye.config")
-                .addPackage("io.smallrye.config")
-
-                // Bean archive deployment descriptor
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
-
     @Test
-    public void testHealth() throws Exception {
+    public void testHealthChecks() throws Exception {
         CamelContext camelContext = new DefaultCamelContext();
-        camelContext.addRoutes(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                from("direct:start")
-                    .throwException(new Exception());
-            }
-        });
-        camelContext.getRegistry().bind("dummy", dummyHealthCheck);
-        CamelMicroProfileHealthCheck check = new CamelMicroProfileHealthCheck();
-        check.setCamelContext(camelContext);
-
-        SmallRyeHealthReporter reporter = new SmallRyeHealthReporter();
-        reporter.addHealthCheck(check);
 
         HealthCheckRegistry healthCheckRegistry = HealthCheckRegistry.get(camelContext);
 
+        ContextHealthCheck contextHealthCheck = new ContextHealthCheck();
+        contextHealthCheck.setCamelContext(camelContext);
+        contextHealthCheck.getConfiguration().setEnabled(true);
+//        healthCheckRegistry.register(contextHealthCheck);
+
+        CustomHealthCheck customHealthCheck = new CustomHealthCheck();
+        customHealthCheck.getConfiguration().setEnabled(true);
+        healthCheckRegistry.register(customHealthCheck);
+
         RoutesHealthCheckRepository repository = new RoutesHealthCheckRepository();
-        healthCheckRegistry.addRepository(repository);
-        repository.addEvaluator(new RoutePerformanceCounterEvaluators.ExchangesFailed(1));
-//        repository.addEvaluator(new RoutePerformanceCounterEvaluators.LastProcessingTime(1000, 1));
+//        healthCheckRegistry.addRepository(repository);
+
+        SmallRyeHealthReporter reporter = new SmallRyeHealthReporter();
+        CamelMicroProfileHealthCheck microProfileHealthCheck = new CamelMicroProfileHealthCheck();
+        microProfileHealthCheck.setCamelContext(camelContext);
+        reporter.addHealthCheck(microProfileHealthCheck);
 
         camelContext.start();
         try {
-            ProducerTemplate template = camelContext.createProducerTemplate();
-
-            for (int i = 0; i < 2; i++) {
-                try {
-                    template.sendBody("direct:start", null);
-                } catch (Throwable t) {
-                }
-            }
-
+//            camelContext.getRouteController().stopRoute("route1");
             SmallRyeHealth health = reporter.getHealth();
             reporter.reportHealth(System.out, health);
         } finally {
